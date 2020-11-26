36cf93ed8fc6f180131f88424d49c1bb13cc9013
evelyn-sz <evelynszarek@gmail.com>
2020-11-26 14:05:47 +0000

    Test 1 passed

--- src/task.py
+++ src/task.py
@@ -1,4 +1,4 @@
-class Task():
+class Task:
 
     def __init__(self, name, duration):
         self.name = name
--- tasks/task_test.py
+++ tasks/task_test.py
@@ -4,9 +4,16 @@ from src.task import Task
 
 class TestTask(unittest.TestCase):
 
-# - "Wash Dishes" is preferred over "Cook Dinner"
-    def test_wash_dishes_preferred_over_cook_dinner(self):
-        self.assertEqual("Wash Dishes", task())
+    def setUp(self):
+        self.task_1 = Task("Wash Dishes", 15)
 
-# - "Cook Dinner" is preferred over "Clean Windows"
-# - "Clean Windows is preferred over "Wash Dishes"
+
+    def test_task_has_name(self):
+        self.assertEqual("Wash Dishes", self.task_1.name)
+
+    # - "Wash Dishes" is preferred over "Cook Dinner"
+    # def test_wash_dishes_preferred_over_cook_dinner(self):
+    #     self.assertEqual("Wash Dishes", self.task_1())
+
+    # - "Cook Dinner" is preferred over "Clean Windows"
+    # - "Clean Windows is preferred over "Wash Dishes"
