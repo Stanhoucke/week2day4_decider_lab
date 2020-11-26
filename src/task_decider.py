639a35dd2640108f2e323aa14576a70bc6376274
evelyn-sz <evelynszarek@gmail.com>
2020-11-26 14:40:07 +0000

    Test 3 passed

--- src/task_decider.py
+++ src/task_decider.py
@@ -1,5 +1,7 @@
+from src.task import Task
+
 def get_preferred_option(task_1, task_2):
-    if task_1 == "Wash Dishes":    
+    if task_1.name == "Wash Dishes" or task_1.name == "Cook Dinner":    
         return task_1
-    else:
-        return task_2
+    # elif task_2.name == "Cook Dinner":
+    #     return task_2
--- tasks/task_test.py
+++ tasks/task_test.py
@@ -8,6 +8,7 @@ class TestTask(unittest.TestCase):
     def setUp(self):
         self.task_1 = Task("Wash Dishes", 15)
         self.task_2 = Task("Cook Dinner", 30)
+        self.task_3 = Task("Clean Windows", 20)
 
 
     def test_task_has_name(self):
@@ -15,7 +16,11 @@ class TestTask(unittest.TestCase):
 
     # - "Wash Dishes" is preferred over "Cook Dinner"
     def test_wash_dishes_preferred_over_cook_dinner(self):
-        self.assertEqual("Wash Dishes", get_preferred_option(self.task_1.name, self.task_2.name))
+        self.assertEqual(self.task_1, get_preferred_option(self.task_1, self.task_2))
 
     # - "Cook Dinner" is preferred over "Clean Windows"
+
+    def test_cook_dinner_preferred_over_clean_windows(self):
+        self.assertEqual(self.task_2, get_preferred_option(self.task_2, self.task_3))
+        
     # - "Clean Windows is preferred over "Wash Dishes"
