5cf245a0366ecdec0844d35fba81fe60affed2db
evelyn-sz <evelynszarek@gmail.com>
2020-11-26 15:08:55 +0000

    Conditionals and all tests completed

--- src/task_decider.py
+++ src/task_decider.py
@@ -1,7 +1,31 @@
 from src.task import Task
 
-def get_preferred_option(task_1, task_2):
-    if task_1.name == "Wash Dishes" or task_1.name == "Cook Dinner" or task_1.name == "Clean Windows":    
-        return task_1
+# def get_preferred_option(task_1, task_2):
+#     if task_1.name == "Wash Dishes" or task_1.name == "Cook Dinner" or task_1.name == "Clean Windows":    
+#         return task_1
     # elif task_2.name == "Cook Dinner":
     #     return task_2
+
+def get_preferred_option(task_1, task_2):
+    if task_1.name == "Wash Dishes" and task_2.name == "Cook Dinner":
+        return task_1
+    elif task_1.name == "Cook Dinner" and task_2.name == "Wash Dishes":
+        return task_2
+    elif task_1.name == "Cook Dinner" and task_2.name == "Clean Windows":
+        return task_1
+    elif task_1.name == "Clean Windows" and task_2.name == "Cook Dinner":
+        return task_2
+    elif task_1.name == "Clean Windows" and task_2.name == "Wash Dishes":
+        return task_1
+    elif task_1.name == "Wash Dishes" and task_2.name == "Clean Windows":
+        return task_2
+    else:
+        return "Undefined task"
+
+    # [task_1, task_2]
+
+    # ["Wash Dishes", "Cook Dinner"]
+
+    # ["Cook Dinner", "Clean Windows"]
+
+    # ["Clean Windows", "Wash Dishes"]
--- tasks/task_test.py
+++ tasks/task_test.py
@@ -17,13 +17,24 @@ class TestTask(unittest.TestCase):
     # - "Wash Dishes" is preferred over "Cook Dinner"
     def test_wash_dishes_preferred_over_cook_dinner(self):
         self.assertEqual(self.task_1, get_preferred_option(self.task_1, self.task_2))
+    
+    def test_cook_dinner_not_preferred_over_wash_dishes(self):
+        self.assertEqual(self.task_1, get_preferred_option(self.task_2, self.task_1))
 
     # - "Cook Dinner" is preferred over "Clean Windows"
 
     def test_cook_dinner_preferred_over_clean_windows(self):
         self.assertEqual(self.task_2, get_preferred_option(self.task_2, self.task_3))
 
+    def test_clean_windows_not_preferred_over_cook_dinner(self):
+        self.assertEqual(self.task_2, get_preferred_option(self.task_3, self.task_2))
+
     # - "Clean Windows is preferred over "Wash Dishes"
     
     def test_clean_windows_preferred_over_wash_dishes(self):
         self.assertEqual(self.task_3, get_preferred_option(self.task_3, self.task_1))
+
+    def test_wash_dishes_not_preferred_over_clean_windows(self):
+        self.assertEqual(self.task_3, get_preferred_option(self.task_1, self.task_3))
+
+    
